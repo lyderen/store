9ccad2a7ed2c6ffe9950f9a4e790bf2b3b9f4714
lyderen@gmail.com <lyderen@gmail.com>
2020-01-30 00:06:44 +0200

    second

--- cartServ.js
+++
@@ -1,7 +0,0 @@
-var Servic = angular.module('myApp', []);
-Servic.factory('cartService', function() {
-  var cart = [];
-  // factory function body that constructs shinyNewServiceInstance
-
-  return cart;
-});
